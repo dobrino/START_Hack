59d9595fe27c0867bd81f4e66ca4e45d319179af
Carmen Berndt <carmen.berndt@tum.de>
2022-03-24 17:10:36 +0100

    init neural net

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = b'mangomango'
 
-#app routing to url
+# app routing to url
 @app.route("/mango")
 def index():
     flash("Flashy flashy")
---
+++ src/train.py
@@ -0,0 +1,4 @@
+
+
+def train():
+    print('TODO')
