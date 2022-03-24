ebb165a2cba46be155af41dcf15452c16a726c69
Carmen Berndt <carmen.berndt@tum.de>
2022-03-24 10:46:06 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # START_Hack
 Alles Mango
+
+
+- Hit `flask run`
+- Go to http://127.0.0.1:5000/mango
---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = b'mangomango'
+
+#app routing to url
+@app.route("/mango")
+def index():
+    flash("Flashy flashy")
+    return render_template("index.html")
