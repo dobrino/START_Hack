4e811839d036e7b0144d36b62e1f3dbf20dcb6a2
zhenghk <zhenghk@yahoo.de>
2022-03-25 04:44:36 +0100

    setup webapp

--- testing123.py
+++ testing123.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
+import app
+import calculations as c
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html", peak_power = c.get_peak_power(50))
+
+
 
 if __name__ == '__main__':
     app.run(port=5000)
